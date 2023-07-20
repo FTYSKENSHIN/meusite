274cce0362457c9482385a05fade85103f127170
FTYSKENSHIN <77653248+FTYSKENSHIN@users.noreply.github.com>
2023-07-20 19:54:23 -0300

    Update calculo.js

--- calculo.js
+++ calculo.js
@@ -1,12 +1,23 @@
-// Código JavaScript para o cálculo de calorias
+const botaoCalcular = document.getElementById("botao-calcular");
+const velocidadeInput = document.getElementById("velocidade");
+const tempoInput = document.getElementById("tempo");
+const pesoInput = document.getElementById("peso");
+const resultado = document.getElementById("resultado");
 
-function calculaCalorias() {
-    var p = parseFloat(peso.value);
-    var v = parseFloat(velocidade.value);
-    var t = parseFloat(tempo.value);
+botaoCalcular.addEventListener("click", () => {
+  const velocidade = parseFloat(velocidadeInput.value);
+  const tempo = parseFloat(tempoInput.value);
+  const peso = parseFloat(pesoInput.value);
 
-    var resultado = p * v * t * 0.0175;
-   
-    document.getElementById("resultado").innerHTML = "Seu gasto calorico foi de " + resultado + " calorias";
+  if (isNaN(velocidade) || isNaN(tempo) || isNaN(peso)) {
+    resultado.textContent = "Por favor, insira valores válidos.";
+  } else {
+    const calorias = calcularCalorias(velocidade, tempo, peso);
+    resultado.textContent = "Resultado: " + calorias + " Kcalorias!";
   }
-  
+});
+
+function calcularCalorias(velocidade, tempo, peso) {
+  var calorias = velocidade * peso * tempo * 0.0175;
+  return calorias;
+}
